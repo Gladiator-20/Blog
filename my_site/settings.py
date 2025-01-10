f98a75711b7880266dd8184f89b78589e714878c
Aashutosh <parohaaashutosh@gmail.com>
2025-01-11 02:37:19 +0530

    updated requirements.txt

--- my_site/settings.py
+++ my_site/settings.py
@@ -117,20 +117,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_ROOT = BASE_DIR/"staticfiles"
+STATIC_ROOT = BASE_DIR/"staticfiles/blog"
 STATIC_URL = "static/"
 
 # STATICFILES_DIRS = [
 #     BASE_DIR/"static"
 # ]
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
-MEDIA_ROOT = BASE_DIR/"staticfiles"
+MEDIA_ROOT = BASE_DIR/"uploads"
 MEDIA_URL = "/files/"
 
 # Default primary key field type
