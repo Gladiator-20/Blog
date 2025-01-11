c52180ad2ad0cb71cd96e157ff6b5ff5531c05fe
Aashutosh <parohaaashutosh@gmail.com>
2025-01-11 22:14:01 +0530

    updated # Install dependencies

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,8 @@
 # Print a message to indicate the script is running
 echo "Running collectstatic..."
 
+# Install dependencies
+pip install -r requirements.txt
+
 # Run Django's collectstatic command
 python3 manage.py collectstatic --noinput
